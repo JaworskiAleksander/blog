e0822625eee473a6822da732696a476964151742
Aleksander Jaworski <aleck156@gmail.com>
2020-05-17 11:18:51 +0200

    created PostListView, connected with models.Post, and set up get_queryset to return only old posts, sorted by poblished_date in descending order

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import (TemplateView)
+from django.views.generic import (TemplateView, ListView)
+from blog.models import Post, Comment
+from django.utils import timezone
 
 # Create your views here.
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+class PostListView(ListView):
+    model = models.Post
+
+    # this allows to use django ORL
+    def get_queryset(self):
+        # get all objects whose published_date field is less than or equal to timezone.now()
+        # and filter it by publish_date field in descending order
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
