3c1fa6a5aee147b4ec02b004340fb04b3dce406a
Aleksander Jaworski <aleck156@gmail.com>
2020-05-17 10:25:36 +0200

    added PostForm, with Meta class connecting to proper model class and setting editable fields to author, title and text

--- blog_project/blog/forms.py
+++ blog_project/blog/forms.py
@@ -1,4 +1,13 @@
 from django import forms
 from blog.models import Post, Comment
 
+class PostForm(forms.ModelForm):
+
+    class Meta():
+        # connect with a proper model claasss
+        model = Post
+        # connect the fields you'll be able to edit
+        # why tuples here? because they're IMMUTABLE
+        fields = ('author', 'title', 'text')
+
 
