c13558690271f52922b9ab1d7523b5f284ce66d6
Aleksander Jaworski <aleck156@gmail.com>
2020-05-17 11:31:22 +0200

    created postDetailView

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import (TemplateView, ListView)
+from django.views.generic import (TemplateView, ListView, DetailView)
 from blog.models import Post, Comment
 from django.utils import timezone
 
@@ -15,3 +15,6 @@ class PostListView(ListView):
         # get all objects whose published_date field is less than or equal to timezone.now()
         # and filter it by publish_date field in descending order
         return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
