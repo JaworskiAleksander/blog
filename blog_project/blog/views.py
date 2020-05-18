ad44da86c24615c7aae8eee042fc2eec38af9866
Aleksander Jaworski <aleck156@gmail.com>
2020-05-18 10:29:58 +0200

    defined add_comment_to_post function view, added login_required decorator

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -75,5 +75,19 @@ class Drafts(LoginRequiredMixin, ListView):
 
 #----------------------------------------
 # Drafts
+
+@login_required(login_url='/login/')
 def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
     pass
