322358ed059e5eb7b7f441dba0515369127889e1
Aleksander Jaworski <aleck156@gmail.com>
2020-05-18 10:32:51 +0200

    reformatted urlaptterns with comments for better readability

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -2,12 +2,17 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
+    # main views
     path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
+
+    # post views
     path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
     path('drafts/', views.Drafts.as_view(), name='drafts'),
+
+    # comments
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 ]
