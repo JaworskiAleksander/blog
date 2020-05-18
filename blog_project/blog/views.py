99ca4a9f843941fb2dcdfe51cfa102829fa90522
Aleksander Jaworski <aleck156@gmail.com>
2020-05-18 10:37:45 +0200

    defined comment_approve function view

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -94,4 +94,6 @@ def add_comment_to_post(request, pk):
 
 @login_required(login_url='login')
 def comment_approve(request,pk):
-    pass
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
